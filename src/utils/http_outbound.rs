b8adcae6112c81174a23302775969137ad65676c
PatrickStat555 <PatrickStar9846@proton.me>
2026-08-03 00:33:47 +0800

    change ua

--- src/utils/http_outbound.rs
+++ src/utils/http_outbound.rs
@@ -103,7 +103,7 @@ async fn request_once_with_body(
         .method(method)
         .uri(path)
         .header("Host", host)
-        .header("User-Agent", user_agent.unwrap_or("clash/sing-box/0.1"))
+        .header("User-Agent", user_agent.unwrap_or("quicproxy/sing-box/clash"))
         .header("Accept", "*/*")
         .header("Accept-Encoding", "identity")
         .header("Connection", "close")
